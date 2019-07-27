a70af8abeb06f4424b20e17f9c73d252745b3ac3
Chris Pua <chris.pua@mail.utoronto.ca>
2019-07-27 12:28:20 -0400

    Additional functionality to retrieve relevant pages and added more comments

--- main.py
+++ main.py
@@ -10,29 +10,45 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-url_link = "https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm"
-page = requests.get(url_link)
+""" 
+! CHECK THE WEBISTE FOR TERMS AND CONDITIONS, AND TO SEE IF THEY HAVE AN API THAT ALLOWS YOU TO GRAB DATA.
+! IT'S ALSO A GOOD IDEA TO SCRAPE WITH A HEADER THAT HAS YOUR NAME AND EMAIL
+""" 
 
-soup = BeautifulSoup(page.text, 'html.parser')
-
-# Removing unwanted links
-last_links = soup.find(class_='AlphaNav')
-last_links.decompose()
-
-# Find div class containing list of artist names 
-artist_name_list = soup.find(class_='BodyText')
-# In the div class, find all <a> tags, which are links 
-artist_name_list_items = artist_name_list.find_all('a')
+headers = {
+    'User-Agent': 'Chris Pua, web.archive.org',
+    'From': 'chris.pua@mail.utoronto.ca'
+}
 
 f = csv.writer(open('z-artist-names.csv', 'w'))
 f.writerow(['Name', 'Link'])
 
-for artist_name in artist_name_list_items:
-    names = artist_name.contents[0]
-    # Using getting to retrieve variable 'href' value
-    links = 'https://web.archive.org/' + artist_name.get('href')
+pages = []
+# In the URL link, the number after the 'anZ' changes when you switch pages 
+for i in range (1,5):
+    url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ' + str(i) + '.htm'
+    pages.append(url)
     
-    # Add each artist's name and associated link to a row
-    f.writerow([names,links])
-    
+for item in pages:
+    page = requests.get(item, headers = headers)
+    soup = BeautifulSoup(page.text, 'html.parser')
 
+    # Removing unwanted links
+    last_links = soup.find(class_='AlphaNav')
+    last_links.decompose()
+
+    # Find div class containing list of artist names 
+    artist_name_list = soup.find(class_='BodyText')
+    # In the div class, find all <a> tags, which are links 
+    artist_name_list_items = artist_name_list.find_all('a')
+
+    for artist_name in artist_name_list_items:
+        names = artist_name.contents[0]
+        # Using getting to retrieve variable 'href' value
+        links = 'https://web.archive.org/' + artist_name.get('href')
+        
+        # Add each artist's name and associated link to a row
+        f.writerow([names,links])
+        
+
+    
